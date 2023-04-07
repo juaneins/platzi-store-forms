4f6496b6a06c5f91160d2a2569c5c2b1be7f3bc1
juaneins_uio <ballesterosjg@gmail.com>
2023-04-07 18:28:42 -0500

    dynamic forms with formArray

--- src/app/order/components/order/order.component.ts
+++ src/app/order/components/order/order.component.ts
@@ -4,23 +4,56 @@ import { Observable } from 'rxjs';
 
 import { Product } from './../../../core/models/product.model';
 import { CartService } from './../../../core/services/cart.service';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.scss']
+  styleUrls: ['./order.component.scss'],
 })
 export class OrderComponent implements OnInit {
-
   products$: Observable<Product[]>;
+  form: FormGroup;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) {
     this.products$ = this.cartService.cart$;
+    this.buildForm();
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      address: this.formBuilder.array([]),
+    });
   }
 
+  addAddressField() {
+    this.addressField.push(this.createAddressField());
+  }
+
+  private createAddressField() {
+    return this.formBuilder.group({
+      zip: ['', Validators.required],
+      text: ['', Validators.required],
+    });
+  }
+
+  get addressField() {
+    return this.form.get('address') as FormArray;
+  }
+
+  save() {
+    console.log(this.form.value);
+  }
 }
